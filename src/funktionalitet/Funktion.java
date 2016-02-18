7caebfe499ae53a541f7ed937b2e51cd9fa3f43f
Magnus Haakonsson <magnusmisbat@gmail.com>
2016-02-18 15:27:56 +0100

    Lavet OP_Data objektet i funktionalitet's klassen

--- src/funktionalitet/Funktion.java
+++ src/funktionalitet/Funktion.java
@@ -1,6 +1,44 @@
 package funktionalitet;
 
+import data.OP_Data;
+
 public class Funktion {
+	
+	private OP_Data OP_Data;
+	
+	public Funktion(OP_Data OP_Data){
+
+		this.OP_Data = OP_Data;
+		
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 
 
