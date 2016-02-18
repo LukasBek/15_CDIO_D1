d4daaaa418b158cea3a7083572cc71003e925d98
Magnus Haakonsson <magnusmisbat@gmail.com>
2016-02-18 16:05:34 +0100

    Tilføjede ting i funktionalitet

--- src/funktionalitet/Funktionalitet.java
+++ src/funktionalitet/Funktionalitet.java
@@ -1,11 +1,37 @@
 package funktionalitet;
 
+import data.OP_Data;
+
 public class Funktionalitet implements IFunktionalitet {
 
+	private OP_Data OP_Data;
+	
+	public Funktionalitet(OP_Data OP_Data) {
+		
+		this.OP_Data = OP_Data;
+	
+	}
+	
+	
+
+	
+	
+	
 	@Override
 	public boolean tjekOPLogin(int ID, String pass) {
 		// TODO Auto-generated method stub
 		return false;
 	}
+	
+	public void ThisDoStuff(int index){
+		
+		OP_Data.getCpr(index);
+		OP_Data.getIni(index);
+		OP_Data.getOprNavn(index);
+		OP_Data.getPassword(index);
+		OP_Data.getOprId(index);
+		
+	}
+	
 
 }
