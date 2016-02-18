777b02597b9332cda3f0ca0edf06b2f5501a9025
Big Fat Llama <lukas@10.16.99.208>
2016-02-18 15:57:07 +0100

    Rettet fejl

---
+++ src/funktionalitet/Funktionalitet.java
@@ -0,0 +1,11 @@
+package funktionalitet;
+
+public class Funktionalitet implements IFunktionalitet {
+
+	@Override
+	public boolean tjekOPLogin(int ID, String pass) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
