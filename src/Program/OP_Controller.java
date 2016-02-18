3d4d218ad7660412b537540b147d72f920f88ec1
Magnus Haakonsson <magnusmisbat@gmail.com>
2016-02-18 14:38:00 +0100

    Tilføjet resterende metoder i OPController

--- src/Program/OP_Controller.java
+++ src/Program/OP_Controller.java
@@ -16,23 +16,44 @@ public class OP_Controller {
 		
 	}
 	
+	/**
+	 * @param index : the place in the array list the method will be called
+	 * @return The CPR.nr of the operator
+	 */
+	public String getCpr(int index){
+		return (opArray.get(index)).getCpr();
+	}
 
-/**
- * Will return the ID of the operator on the given index
- * @param index the place in the array list the method will be called
- * @return The ID of the operator
- */
-	
-	public int getOprId(int index){
-	
-		return (opArray.get(index)).getOprId();
-	
+	/**
+	 * @param index : the place in the array list the method will be called
+	 * @return The Ini (?) of the operator
+	 */
+	public String getIni(int index){
+		return (opArray.get(index)).getIni();
 	}
 	
+	/**
+	 * @param index : the place in the array list the method will be called
+	 * @return The Name of the operator
+	 */
+	public String getOprNavn(int index){
+		return (opArray.get(index)).getOprNavn();
+	}	
 	
+	/**
+	 * @param index : the place in the array list the method will be called
+	 * @return The Password of the operator
+	 */
+	public String getPassword(int index){	
+		return (opArray.get(index)).getPassword();
+	}
 	
-	
-	
-	
+	/**
+	 * @param index : the place in the array list the method will be called
+	 * @return The ID of the operator
+	 */
+	public int getOprId(int index){
+		return (opArray.get(index)).getOprId();
+	}
 
 }
