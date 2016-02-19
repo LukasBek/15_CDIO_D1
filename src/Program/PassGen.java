3f39d7e4bc643d42295ba1b206793f39a71b8635
Quaade94 <Quaade94@82.211.211.9>
2016-02-19 20:04:37 +0100

    Flytter Password Generatoren ind i vores program og har skrevet kommentarer i koden.

--- src/Program/PassGen.java
+++ src/Program/PassGen.java
@@ -1,4 +1,4 @@
-package TEMPMAIN;
+package Program;
 
 import java.util.ArrayList;
 
@@ -11,7 +11,9 @@ public class PassGen {
 	private boolean tal;
 	private boolean tegn;
 	private int tilf;
+	private int passLaengde;
 
+	//Metoden opretter et array med alle de mulige cifre til det tilfaeldige password.
 	public void array(){
 		for (int i = 0; i<=9 ; i++){
 			karakterer.add(Integer.toString(i));
@@ -35,28 +37,38 @@ public class PassGen {
 	//store bogstaver ved index 	36-61
 	//tegn ved index 				62-68
 
-	public String passGen(){
+	public void passGen(){
+		//Opretter arrayet
 		array();
+		
+		//Hvor langt skal password være? (krav paa min 6)
+		passLaengde = 6;
 
-		for(int i=0 ; password.length()<6 ; i++){
-			//hvis alle er true bliver de alle false
+		while(password.length()<passLaengde){
+			//2. (se 1. længere nede) Naar de alle er blacklisted, bliver de alle whitelisted igen.
 			if(smaaBogstaver && storeBogstaver && tal && tegn){
 				smaaBogstaver=false;
 				storeBogstaver=false;
 				tal=false;
 				tegn=false;
 			}
+			//Generer et tilfaeldigt tal som bruges til arrayet.
 			tilf = (int) (Math.random()*69);
+			
+			//Indsaetter værdien i koden hvis den er whitelisted.
 			if(tilf<=9&&!tal)							password = password + karakterer.get(tilf);
 			if(tilf>=10 && tilf<=35&&!smaaBogstaver)	password = password + karakterer.get(tilf);
 			if(tilf>=36 && tilf<=61&&!storeBogstaver)	password = password + karakterer.get(tilf);
 			if(tilf>=62 && tilf<=68&&!tegn)				password = password + karakterer.get(tilf);
-
+			
+			//1. For at der er 1 af hver 4 mulige tegn bliver de blacklisted her 1 af gangen.
 			if(tilf<=9)tal=true;
 			if(tilf>=10 && tilf<=35)smaaBogstaver=true;
 			if(tilf>=36 && tilf<=61)storeBogstaver=true;
 			if(tilf>=62 && tilf<=68)tegn=true;
 		}
+	}
+	public String getRandomPassword(){
 		return password;
 	}
 }
