d147027e9b10565cb27207a3b079517e4343255a
Big Fat Llama <lukas@10.16.99.208>
2016-02-22 10:30:10 +0100

    Ændret i grænslefladen

--- src/controllere/ProgramController.java
+++ src/controllere/ProgramController.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 import funktionalitet.Funktionalitet;
 
-public class loginController {
+public class ProgramController {
 	
 	private Scanner sc;
 	private Funktionalitet f;
 	
 	int valg;
 	
-	public loginController(Scanner sc, Funktionalitet f) {
+	public ProgramController(Scanner sc, Funktionalitet f) {
 		this.sc=sc;
 		this.f=f;
 	}
@@ -25,7 +25,12 @@ public class loginController {
 			int ID = sc.nextInt();
 			System.out.println("Indtast Adgangskode: ");
 			String kode = sc.nextLine();
-			f.tjekLogin(ID, kode, 1);
+			if(f.tjekLogin(ID, kode, 1)){
+				operatørMenu();
+			}else{
+				
+			}
+		
 
 		}
 		if (valg == 2){
@@ -33,12 +38,24 @@ public class loginController {
 			int ID = sc.nextInt();
 			System.out.println("Indtast Adgangskode: ");
 			String kode = sc.nextLine();
-			f.tjekLogin(ID, kode, 2);
+			if(f.tjekLogin(ID, kode, 2)){
+				sysAdminMenu();
+			}
 		}
 		else{
 			System.out.println("Du skal vælge noget mellem 1 og 2: ");
 			int nytValg = sc.nextInt();
 			valg(nytValg);
 		}
+	}
+
+	private void sysAdminMenu() {
+		System.out.println("Velkommen operatør")
+		
+	}
+
+	private void operatørMenu() {
+		// TODO Auto-generated method stub
+		
 	}
 }
